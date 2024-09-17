eb4d4f4da4b93e1255c2bda2aba4f429780be9b6
noakatz465 <noakatz465@gmail.com>
2024-09-17 18:39:03 +0300

    Doubling the ticket for each city

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,37 +1,62 @@
 import React, { FC, useEffect, useState } from 'react';
-// import './Home.scss';
 import axios from 'axios';
 import { WeatherModel } from '../../models/WeatherModel.model';
 import Card from '../Card/Card';
 
+interface HomeProps {}
 
-interface HomeProps { }
+const Home: FC<HomeProps> = (props) => {
+  const [weatherData, setWeatherData] = useState<{ [city: string]: WeatherModel | null }>({
+    Eilat: null,
+    London: null,
+    NewYork: null,
+    Alaska: null
+  });
 
-const Home = (props: HomeProps) => {
-  const [weatherData, setWeatherData] = useState<WeatherModel | null>(null);
+  const cities = ['Eilat', 'London', 'New York', 'Alaska'];
 
   useEffect(() => {
     const apiKey = '8ee633956bad6ae1965b557a94ecfcba';
-    axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?q=Eilat&units=metric&appid=${apiKey}&lang=he`)
-      .then((response) => setWeatherData(response.data))
-      .catch((error) => console.error(error));
+
+    // יצירת פונקציה פנימית להבטחת שימוש נכון במשתנה city
+    const fetchWeather = (city: string) => {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=he`)
+        .then((response) => {
+          setWeatherData((prevState) => ({
+            ...prevState,
+            [city]: response.data
+          }));
+        })
+        .catch((error) => console.error(`Error fetching weather data for ${city}:`, error));
+    };
+
+    cities.forEach((city) => {
+      fetchWeather(city);
+    });
   }, []);
 
-  return <div className="Home">
-    <h1>מזג האוויר בערים</h1>
-    {weatherData ? (
-      <Card
-        cityName="אילת"
-        temp={weatherData.main.temp}
-        feels_like={weatherData.main.feels_like}
-        humidity={weatherData.main.humidity}
-        description={weatherData.weather[0].description}
-      />
-    ) : (
-      <p>טוען נתונים...</p>
-    )}
-  </div>
-}
+  return (
+    <div className="Home">
+      <h1>מזג האוויר בערים</h1>
+      <div className="cards-container">
+        {cities.map((city) =>
+          weatherData[city] ? (
+            <Card
+              key={city}
+              cityName={city}
+              temp={weatherData[city]!.main.temp}
+              feels_like={weatherData[city]!.main.feels_like}
+              humidity={weatherData[city]!.main.humidity}
+              description={weatherData[city]!.weather[0].description}
+            />
+          ) : (
+            <p key={city}>טוען נתונים עבור {city}...</p>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
